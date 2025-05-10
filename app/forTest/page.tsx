4ea7729e733a34e7ac18eb478653d95a3c533bfd
gan15932156 <powerganoflove@hotmail.com>
2025-05-10 16:32:27 +0700

    update: test file

--- app/forTest/page.tsx
+++ app/forTest/page.tsx
@@ -1,6 +1,11 @@
-import React from "react";
-import chroma from "chroma-js";
-const Page = () => {
+import { promises as fs } from "fs";
+const Page = async () => {
+  const file = await fs.readFile(
+    process.cwd() + "/app/data/style-properties.json",
+    "utf8"
+  );
+  const data = JSON.parse(file);
+  console.log(data);
   return <div>Page</div>;
 };
 
